1732816fc815a9296afbed07f8df4e06b7540977
sudharsan8621 <sudharsan8621@gmail.com>
2026-01-09 13:25:14 +0530

    Fix CORS

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const app = express();
 
 // CORS
 app.use(cors({
-    origin: '*',
+    origin: ['https://task-tracker-six-umber.vercel.app/', 'http://localhost:5173'],,
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
